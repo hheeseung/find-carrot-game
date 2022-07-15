67a453e79a263626765942c3c6e170b4d150b864
hheeseung <ssigu69@gmail.com>
2022-07-15 14:50:48 +0900

    HTML: Replay, JS: Stop event

--- main.js
+++ main.js
@@ -1,30 +1,43 @@
 const play = document.querySelector('.game-control__play');
 const stop = document.querySelector('.game-control__stop');
 const timer = document.querySelector('.game-control__timer');
+const replay = document.querySelector('.replay');
+const field = document.querySelector('.game-field');
 const bgm = new Audio('/sound/bg.mp3');
-const invalid = null;
+const stopEffect = new Audio('/sound/alert.wav');
 const HIDDEN_CLASSNAME = 'hidden';
 
+// 10초 카운트다운
+let leftTime = 10;
+let playTimer;
+const countdown = () => {
+  if (leftTime < 10) {
+    timer.innerText = `00:0${leftTime}`;
+  } else {
+    timer.innerText = `00:${leftTime}`;
+  }
+  leftTime -= 1;
+  if (leftTime < 0) {
+    clearInterval(playTimer);
+    bgm.pause();
+  }
+};
+
+// ▶ 버튼 클릭 시 이벤트
 play.addEventListener('click', () => {
   bgm.play();
   play.classList.add(HIDDEN_CLASSNAME);
   stop.classList.remove(HIDDEN_CLASSNAME);
-
-  let leftTime = 10;
-  setInterval(() => {
-    if (leftTime < 10) {
-      timer.innerText = `00:0${leftTime}`;
-    } else {
-      timer.innerText = `00:${leftTime}`;
-    }
-    leftTime -= 1;
-    if (leftTime < 0) {
-      timer.innerText = `00:00`;
-    }
-  }, 1000);
+  playTimer = setInterval(countdown, 1000);
 });
 
+// ⏹ 버튼 클릭 시 이벤트
 stop.addEventListener('click', () => {
   stop.style.visibility = HIDDEN_CLASSNAME;
   bgm.pause();
+  clearInterval(playTimer);
+  stopEffect.play();
+  replay.classList.remove(HIDDEN_CLASSNAME);
 });
+
+function setCarrot() {}
